f79c74e508c2837cd2e475bc528b1a0075215569
evert <everton.junio132@gmail.com>
2025-09-30 11:37:04 -0300

    Adicionando constante de numero maximo de hidrometros

--- Hidrometro/src/Main.java
+++ Hidrometro/src/Main.java
@@ -7,12 +7,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> {
+            final int NUM_MAXIMO_HIDROMETROS = 5;
             Scanner sc = new Scanner(System.in);
+
             System.out.println("Digite quantos hidrômetros você quer utilizar? (MAX: 5)");
             int quantidadeHidrometros = sc.nextInt();
 
 
-            for (int i = 1; i <= Math.min(quantidadeHidrometros, 5); i++) {
+            for (int i = 1; i <= Math.min(quantidadeHidrometros, NUM_MAXIMO_HIDROMETROS); i++) {
                 System.out.print("Digite a vazão inicial do Hidrômetro " + i + ": ");
                 double vazao = sc.nextDouble();
 
@@ -24,7 +26,7 @@ public class Main {
                 }
             }
 
-            for (int i = 1; i <= (Math.min(quantidadeHidrometros, 5)); i++) {
+            for (int i = 1; i <= (Math.min(quantidadeHidrometros, NUM_MAXIMO_HIDROMETROS)); i++) {
                 String entrada = "vazao" + i + ".txt";
                 String saida = "hidrometro" + i + ".png";
                 HidrometroUI ui = new HidrometroUI("Hidrometro " + i, entrada, saida);
